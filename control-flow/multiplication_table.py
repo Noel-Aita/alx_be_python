d3ae32f0fbfca1fd9eac9157cd401d3e42d53003
Econi Noel Aita <noelaita03@gmail.com>
2025-05-30 10:33:32 +0300

    Iterated the for loop in multiplication_table.py file

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
+
 number = int(input("Enter a number to see its multiplication table: "))
 
 # Show the multiplication table
 #print(f"Multiplication Table for {number}:")
-for i in range(0,  11): # Starts counting from 0 up to 10 excluding number 11 itself
-=======
-number = int(input("Enter a number to see its multiplication table: "))
+for i in range(1,  11): # Starts counting from 0 up to 10 excluding number 11 itself
 
-# Show the multiplication table
-#print(f"Multiplication Table for {number}:")
-for i in range(0,  11): # Starts counting from 0 up to 10 excluding number 11 itself
->>>>>>> c12935383a58d7cb7028c7ab31ae47f303f24622
     print(f"{number} x {i} = {number * i}")
