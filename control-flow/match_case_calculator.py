7cd006388b139a170d6ac06d959d1ae9d8991aac
Econi Noel Aita <noelaita03@gmail.com>
2025-05-30 10:27:06 +0300

    Included case arguments in the file

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,19 +3,22 @@ num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /): ")
 
-if operation == "+":
-            addition = num1 + num2
-            print("The result is ",+ addition)
-
-elif operation == "-":
-            subtraction = num1 - num2
-            print("The result is ",+ subtraction)
-
-elif operation == "*":
-            multiplication = num1 * num2
+match operation :
+        case "*":
+            if operation == "*":
+                multiplication = num1 * num2
             print("The result is ",+ multiplication)
 
-elif operation == "/": 
+        case "-":
+          if operation == "-":
+            subtraction = num1 - num2
+            print("The result is ",+ subtraction)
+        case "+":
+         if operation == "+":
+            addition = num1 + num2
+            print("The result is ",+ addition)
+        
+if operation == "/": 
     if num2 == 0:
         print("Cannot divide by zero.")
     else:
