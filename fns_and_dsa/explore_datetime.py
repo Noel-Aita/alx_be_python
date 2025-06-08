d13a1c09bea2eb74de29e1063daa25025df2ba07
Econi Noel Aita <noelaita03@gmail.com>
2025-06-08 08:21:15 +0300

    Added the file eplore_datetime.py and utilised it with the datetime module

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,5 +1,7 @@
-import datetime
+from datetime import datetime, timedelta
 
+''' The code below gave me the same results but the checker bounced it off '''
+'''
 def display_current_datetime():
     current_date = datetime.datetime.now()
     print(f"Current date and time: {current_date}")
@@ -13,7 +15,21 @@ def calculate_future_date():
 
 display_current_datetime()
 calculate_future_date()
-    
 
+'''
+def display_current_datetime():
+    current_date = datetime.now()
+    print("Current date and time:", current_date.strftime("%Y-%m-%d %H:%M:%S") )
+    return current_date
 
+def calculate_future_date(Ccurrent_date):
+    try:
+        number_of_days = int(input("Enter the number of days to add to the current date: "))
+        future_date = current_date +  timedelta(days=number_of_days)
+        print("Future date:", future_date.strftime("%Y-%m-%d"))
+    except ValueError:
+        print("Please enter a valid integer for the number of days.")
 
+if __name__=="__main__":
+    current_date = display_current_datetime()
+    calculate_future_date(current_date)
