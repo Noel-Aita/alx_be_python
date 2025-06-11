3a4ac0bde93c7cc709c36f9ce1bb4a18af9f8069
Econi Noel Aita <noelaita03@gmail.com>
2025-06-11 09:33:19 +0300

    Corrected type error in the file test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,7 +7,7 @@ class TestSimpleCalculator(unittest.TestCase):
         """Create a calculator instance for each test."""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         """Test the addition method with various inputs."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, -1), -2)
@@ -15,7 +15,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(0, 0), 0)
         self.assertEqual(self.calc.add(2.5, 1.5), 4.0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         """Test ffor checking the  subtraction method."""
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(0, 3), -3)
@@ -23,14 +23,14 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(-3, -3), 0)
         self.assertEqual(self.calc.subtract(2.5, 1.0), 1.5)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         """Test the multiplication method."""
         self.assertEqual(self.calc.multiply(3, 4), 12)
         self.assertEqual(self.calc.multiply(-1, 4), -4)
         self.assertEqual(self.calc.multiply(0, 1000), 0)
         self.assertEqual(self.calc.multiply(1.5, 2), 3.0)
 
-    def test_divide(self):
+    def test_division(self):
         """Test the division method including division by zero."""
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(5, 2), 2.5)
