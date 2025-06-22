c68cda7e25717371ac47ad021cc3326496cc3d8b
Econi Noel Aita <noelaita03@gmail.com>
2025-06-22 19:17:57 +0300

    added the file polymorphism_demo.py

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,26 @@
+import math
+
+# Base Class
+class Shape:
+    def area(self):
+        """The method should be overiden in derived classes."""
+        raise NotImplementedError("Subclasses must implement the area method.")
+
+
+# Derived Class - Rectangle
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+
+# Derived Class - Circle
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
